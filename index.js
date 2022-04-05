86a918c63e02d60b0fa53bb4fca1c9c8b52c805a
Sofiane M <poubellepropre8@gmail.com>
2022-04-05 12:56:35 +0200

    finished

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor(items,length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(x);
+    this.length++;
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.length < pos) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items == '') {
+    throw new Error('EmptySortedList')
+  }else {
+    return this.items[this.items.length - 1];
+  }
+  }
 
-  min() {}
+  min() {
+    if (this.items == '') {
+    throw new Error('EmptySortedList')
+  }else {
+    return this.items[0];
+  }
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
+    or (let i= 0; i < this.items.length; i++){
+      sum += this.items[i];
+    }
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    let sum = 0;
+    if (this.items == '') {
+      throw new Error('EmptySortedList');
+    } else {
+    for (let i= 0; i < this.items.length; i++){
+      sum += this.items[i];
+    }
+    return sum /= this.items.length;
+  }
 }
 
 module.exports = SortedList;
